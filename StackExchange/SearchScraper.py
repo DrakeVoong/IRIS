7b085c1e9d558511ab65b3711e5fff96f7818619
DrakeVoong <drakevoong404@gmail.com>
2023-03-29 23:54:42 -0700

    Improving readability

--- StackExchange/SearchScraper.py
+++ StackExchange/SearchScraper.py
@@ -14,7 +14,7 @@ def build_url(community, tab, page):
     return f"https://{community}.com/questions?sort={tab}&page={page}"
 
 
-def get_questions(url, session):
+def get_questions(df, url, session):
     response = session.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
 
@@ -22,11 +22,19 @@ def get_questions(url, session):
     name_tags = []
     questions = soup.find_all('div', class_='s-post-summary js-post-summary')
 
+    if df is not None:
+        existing_questions = df['title'].tolist()
+    else:
+        existing_questions = []
+        
     for question in questions:
         name_tag = []
 
         title = question.find('a', class_='s-link').get('href')
 
+        if title in existing_questions:
+            continue
+        
         tags = question.find_all('li', class_= 'd-inline mr4 js-post-tag-list-item')
         for tag in tags:
             name_tag.append(tag.text)
@@ -36,7 +44,7 @@ def get_questions(url, session):
 
     return titles, name_tags
 
-def next_page(community, url, session):
+def next_page(df, community, url, session):
     questions = []
     titles = []
     name_tags = []
@@ -45,28 +53,36 @@ def next_page(community, url, session):
         print(f"Page {i} of {community}...")
 
         url = build_url(community, 'MostFrequent', i)
-        new_titles, new_name_tags = get_questions(url, session)
-        titles.extend(new_titles)
-        name_tags.extend(new_name_tags)
-        time.sleep(2)
+        new_titles, new_name_tags = get_questions(df, url, session)
 
-    communities = [community]*len(titles)
-    return communities, titles, name_tags
+        list_community = [community] * len(new_titles)
+
+        new_df = pd.DataFrame(list(zip(list_community, new_titles, new_name_tags)), columns=['community', 'title', 'name_tags'])
+        if os.path.isfile('.//StackExchange/questions.csv'):
+            new_df.to_csv('.//StackExchange/questions.csv', mode='a', header=False, index=False)
+        else:
+            new_df.to_csv('.//StackExchange/questions.csv', mode='w', header=True, index=False)
+
+        time.sleep(5)
+
+
 
 if __name__ == '__main__':
 
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'})
   
-    websites = ['math.stackexchange', 'physics.stackexchange', 'chemistry.stackexchange', 'biology.stackexchange', 'cs.stackexchange', 'stats.stackexchange', 'datascience.stackexchange']
+    websites = ['stackoverflow', 'unix.stackexchange', 'math.stackexchange', 'askubuntu', 'superuser', 'serverfault', 'salesforce.stackexchange', 'tex.stackexchange', 
+                'stats.stackexchange', 'dba.stackexchange', 'gis.stackexchange', 'electronics.stackexchange', 'apple.stackexchange', 'physics.stackexchange',
+                'wordpress.stackexchange', 'english.stackexchange', 'blender.stackexchange', 'datascience.stackexchange', 'softwareengineering.stackexchange',
+                'security.stackexchange', 'gaming.stackexchange', 'rpg.stackexchange', 'diy.stackexchange', 'ell.stackexchange', 'academia.stackexchange',
+                'academia.stackexchange', 'chemistry.stackexchange', '']
     data = []
 
-    for website in websites:
-        communities, titles, name_tags = next_page(website, url, session)
-        data += list(zip(communities, titles, name_tags))
-    df = pd.DataFrame(data, columns=['community', 'title', 'name_tags'])
     if os.path.exists('.//StackExchange/questions.csv'):
-        df.to_csv('.//StackExchange/questions.csv', mode='a', header=False, index=False)
+        df = pd.read_csv('.//StackExchange/questions.csv')
     else:
-      df.to_csv('.//StackExchange/questions.csv', index=False)
-    
+        df = None
+
+    for website in websites:
+        next_page(df, website, url, session)
